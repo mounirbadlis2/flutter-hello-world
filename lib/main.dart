dedad5420675bb111b4244da9b906316fea181b7
mounirbadlis2 <mounir.badlis@univ-constantine2.dz>
2023-07-03 14:19:24 +0100

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Generate',
+      title: 'Text',
       home: RandomWords(),
     );
   }
